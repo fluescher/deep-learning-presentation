efbef07f62bbe227ffe72b385e154b4c426c5a73
Florian Luescher <florian@florianluescher.ch>
2017-02-26 17:08:36 +0100

    Fix typo in run-docker.sh script

--- run-docker.sh
+++ run-docker.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-docker run -it -v ${PWD}/notebook:/notebooks -p 8888:8888 tensorflow/tensorflow:1.0.0-py
+docker run -it -v ${PWD}/notebook:/notebooks -p 8888:8888 tensorflow/tensorflow:1.0.0-py3
+
